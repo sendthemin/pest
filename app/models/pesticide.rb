dcc51db871a4744d35c3372de4c2efe7231057aa
sendthemin <roten66@gmail.com>
2013-07-14 20:54:59 -0400

    login

--- app/models/pesticide.rb
+++ app/models/pesticide.rb
@@ -1,3 +1,3 @@
 class Pesticide < ActiveRecord::Base
-  attr_accessible :active_ingredient, :common_name, :epa_registration, :formulation, :restricted_use, :scientific_name, :signal
+  attr_accessible :active_ingredient, :common_name, :epa_registration, :formulation, :restricted_use, :scientific_name, :signal, :label_url
 end
